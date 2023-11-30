168d44720e17e298a1d9d8b19656808a6ba03921
Dolbnia Anastasia <nastya.1998sos@mail.ru>
2023-11-30 10:51:42 +0300

    добавила задачу 1

---
+++ Задача 1 корректная/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+//Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+
+// Запросили длиину массива
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+
+//обозначаем массив
+int [] array = new int [size];
+Random rand = new Random();
+
+//заполняем массив
+
+for(int i=0;i<size;i++)
+{
+    array[i] = rand.Next(1,100);
+}
+
+//выводим массив
+
+for(int i=0;i<size;i++)
+{
+    Console.Write($"{array[i]}\t");
+}
+
+Console.WriteLine();
+
+//находим кол-во
+int count = 0;
+for(int i=0;i<size;i++)
+{
+    if ((array[i] >= 20) && (array[i] <= 90))
+    {
+        count ++;
+    }
+}
+
+//выводим кол-во элементов
+Console.WriteLine("Количество элементов в диапазоне " + count);
+ 
